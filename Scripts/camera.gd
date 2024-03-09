34bc607ad1a69c5041092adda4c4d444fb1f0981
st1sdan <131540734+st1sdan@users.noreply.github.com>
2024-03-10 00:26:05 +0700

    Delete Scripts directory

--- Scripts/camera.gd
+++
@@ -1,6 +0,0 @@
-extends Camera2D
-
-func _input(event : InputEvent) -> void:
-    if event is InputEventMouseMotion:
-        var _target = event.position - get_viewport().size * 0.1
-        self.position = _target.normalized() * (_target.length()) * 0.01
